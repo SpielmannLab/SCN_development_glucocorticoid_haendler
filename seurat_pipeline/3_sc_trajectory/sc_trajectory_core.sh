eac237df2c13e760bc7cc7a2555589033444f1bb
varunkas <varun.sreenivasan@outlook.com>
2024-03-04 10:30:21 +0100

    Remov yaml files unused in manuscript
    Removed the use of *picked_astrocytes.yaml from the *.core.sh script for
    trajectory analysis. The yaml file was also added to .gitignore

--- seurat_pipeline/3_sc_trajectory/sc_trajectory_core.sh
+++ seurat_pipeline/3_sc_trajectory/sc_trajectory_core.sh
@@ -34,4 +34,4 @@ chmod +x bin/*
 # Submit the Nextflow Script:
 nextflow run sc_trajectory.nf -params-file sc_trajectory_params_neurons.yaml --id ${SCRATCH/"/scratch/"/} -resume
 nextflow run sc_trajectory.nf -params-file sc_trajectory_params_astrocytes_by_annotation.yaml --id ${SCRATCH/"/scratch/"/} -resume
-nextflow run sc_trajectory.nf -params-file sc_trajectory_params_picked_astrocytes.yaml --id ${SCRATCH/"/scratch/"/} -resume
+# nextflow run sc_trajectory.nf -params-file sc_trajectory_params_picked_astrocytes.yaml --id ${SCRATCH/"/scratch/"/} -resume
